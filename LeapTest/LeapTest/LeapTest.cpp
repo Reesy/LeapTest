c2093413e29177a6806ee4750176751eb5169545
James Rees <james.rees@outlook.com>
2015-02-15 23:32:48 +0000

    Changed to Visual Studio, Leap is hard to target for Codeblocks.

---
+++ LeapTest/LeapTest/LeapTest.cpp
@@ -0,0 +1,45 @@
+// LeapTest.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+#include "Leap.h"
+
+using namespace Leap;
+
+class SampleListener : public Listener {
+public:
+	virtual void onConnect(const Controller&);
+	virtual void onFrame(const Controller&);
+};
+
+void SampleListener::onConnect(const Controller& controller) {
+	std::cout << "Connected" << std::endl;
+}
+
+void SampleListener::onFrame(const Controller& controller) {
+	std::cout << "Frame available" << std::endl;
+}
+int main()
+{
+	SampleListener listener;
+	Leap::Controller controller;
+
+  //  Console::WriteLine(L"Hello World");
+
+	controller.addListener(listener);
+	Frame frame = controller.frame();
+
+	ImageList images = frame.images();
+	for (int i = 0; i < 2; i++){
+		Image image = images[i];
+		std::cout << "DING" << std::endl;
+		const unsigned char* image_buffer = image.data();
+
+		}
+	std::cout << "HEllo, world!";
+	std::cin.get();
+
+	controller.removeListener(listener);
+
+    return 0;
+}
